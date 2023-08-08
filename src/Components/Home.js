eb21c4554390cbda3cc761453120a2b7ded2a4ef
femipraize <felixogbangwor@gmail.com>
2023-08-08 11:46:49 +0100

    Sovatech web project

---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar'
+function Home() {
+  return (
+    <div className='home-container'>
+        <Navbar />
+    </div>
+  )
+}
+
+export default Home
